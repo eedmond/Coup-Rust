95aeb105bb8ba674ea96cd37468dfd0bcee5c6f2
Eric Edmond <ericed@google.com>
2022-11-26 17:56:55 -0800

    Separate PlayerData from PlayerController
    This will allow the engine to own the player data and actual values,
    while allowing player controllers (implementations) to have read-only
    access to those values.
    
    This is mostly a proof of concept of how to use traits (aka interfaces).
    The next step will be to try and implement a controller in a different
    library and get it compiled into and used in our game. That will likely
    require some system for implementations to "register" themselves under a
    specific name, then our command line will accept an array of controller
    implementation names and instantiate them dynamically for the game to
    operate with.

--- coup-engine/src/engine.rs
+++ coup-engine/src/engine.rs
@@ -1,16 +1,18 @@
 #![allow(dead_code)]
+use core::fmt::Debug;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
 
+#[derive(Debug)]
 pub enum Action<'a> {
     Unknown,
     /// Provides a callback that takes in a set of cards (which will be random from the deck) and returns 2 cards back to the deck.
     Ambassador {
         select_role: fn([Class; 2]) -> [Class; 2],
     },
-    Assinate {
+    Assassinate {
         target_player: &'a Player,
     },
     Captain {
@@ -19,7 +21,6 @@ pub enum Action<'a> {
     Coup {
         target_player: &'a Player,
     },
-    Diplomat,
     Duke,
     ForeignAid,
     Income,
@@ -40,13 +41,30 @@ pub struct Deck {
     cards: Vec<Class>,
 }
 
-/// The Player class defines an individual player in the game.
-/// 
-/// Determines an action on its turn when notified by the engine.
-/// Will have read-only access to public state of the game.
-pub struct Player {
+#[derive(Debug)]
+struct PlayerData {
     cards: Vec<Class>,
     gold: i32,
+}
+
+pub trait PlayerController {
+    fn init(&self, engine: &Engine);
+    fn get_next_action(&self) -> Action;
+}
+
+impl Debug for dyn PlayerController {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "PlayerController")
+    }
+}
+/// The Player class defines an individual player in the game.
+///
+/// Determines an action on its turn when notified by the engine.
+/// Will have read-only access to public state of the game.
+#[derive(Debug)]
+pub struct Player {
+    data: PlayerData,
+    controller: Box<dyn PlayerController>,
 }
 
 /// The Engine class runs the Coup game.
@@ -61,11 +79,20 @@ pub struct Engine {
     eliminated_players: Vec<Player>,
 }
 
-impl Player {
-    fn new() -> Self {
-        Player {
+impl Default for PlayerData {
+    fn default() -> Self {
+        PlayerData {
             cards: Vec::new(),
             gold: 2,
+        }
+    }
+}
+
+impl Player {
+    fn new(controller: Box<dyn PlayerController>) -> Self {
+        Player {
+            data: PlayerData::default(),
+            controller: controller,
         }
     }
 }
@@ -90,18 +117,19 @@ impl Deck {
 }
 
 impl Engine {
-    pub fn new(num_players: i32) -> Self {
-        if num_players < 2 || num_players > 6 {
+    pub fn new(players: Vec<Box<dyn PlayerController>>) -> Self {
+        if players.len() < 2 || players.len() > 6 {
             panic!(
                 "Invalid number of players specified. Value must be 2-6, got {}.",
-                num_players
+                players.len()
             );
         }
 
         let mut engine = Engine::default();
+
         engine.active_players = Vec::new();
-        for _ in 0..num_players {
-            engine.active_players.push(Player::new());
+        for controller in players {
+            engine.active_players.push(Player::new(controller));
         }
 
         engine.turn_count = 0;
@@ -116,6 +144,12 @@ impl Engine {
             println!("Card: {:?}.", card);
         }
     }
+
+    pub fn print_current_actions(&self) {
+        for player in &self.active_players {
+            println!("First action: {:?}", player.controller.get_next_action());
+        }
+    }
 }
 
 #[cfg(test)]
@@ -124,7 +158,7 @@ mod tests {
 
     #[test]
     fn initializes_correctly() {
-        let engine = Engine::new(5);
+        let engine = Engine::new(create_players(5));
         assert_eq!(engine.active_players.len(), 5);
         assert_eq!(engine.eliminated_players.len(), 0);
         assert_eq!(engine.gold_pool, 30);
@@ -135,12 +169,34 @@ mod tests {
     #[test]
     #[should_panic(expected = "Invalid number of players specified.")]
     fn panic_on_too_few_players() {
-        Engine::new(1);
+        Engine::new(create_players(1));
     }
 
     #[test]
     #[should_panic(expected = "Invalid number of players specified.")]
     fn panic_on_too_many_players() {
-        Engine::new(7);
+        Engine::new(create_players(7));
+    }
+
+    fn create_players(num_players: i32) -> Vec<Box<dyn PlayerController>> {
+        let test_player = TestPlayerController::default();
+
+        let mut players: Vec<Box<dyn PlayerController>> = Vec::new();
+        for _ in 0..num_players {
+            players.push(Box::new(test_player.clone()));
+        }
+
+        players
+    }
+
+    #[derive(Default, Clone)]
+    struct TestPlayerController;
+
+    impl PlayerController for TestPlayerController {
+        fn init(&self, _engine: &Engine) {}
+
+        fn get_next_action(&self) -> Action {
+            Action::Unknown
+        }
     }
 }
--- coup-engine/src/lib.rs
+++ coup-engine/src/lib.rs
@@ -1,8 +1,37 @@
 mod engine;
 
+use crate::engine::Action;
 use crate::engine::Engine;
+use crate::engine::PlayerController;
 
 pub fn run_game(num_players: i32) {
-    let coup_game = Engine::new(num_players);
+    let coup_game = Engine::new(create_players(num_players));
     coup_game.print_deck();
+    coup_game.print_current_actions();
 }
+
+// Temporary code (until we have actual player controller implementations)
+fn create_players(num_players: i32) -> Vec<Box<dyn PlayerController>> {
+    let test_player = TempPlayerController::default();
+
+    let mut players: Vec<Box<dyn PlayerController>> = Vec::new();
+    for _ in 0..num_players {
+        players.push(Box::new(test_player.clone()));
+    }
+
+    players
+}
+
+#[derive(Default, Clone)]
+struct TempPlayerController;
+
+impl PlayerController for TempPlayerController {
+    fn init(&self, _engine: &Engine) {
+
+    }
+
+    fn get_next_action(&self) -> Action {
+        Action::Duke
+    }
+}
+// End temporary code
